09e7790ccbaa7886c1f910580bc582b677bf54db
Biraj Pokharel <Birajpokharel11>
2022-06-10 02:23:28 +0545

    Settings configured for static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [     #points static files location to static folder
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'  #stores user uploaded files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
