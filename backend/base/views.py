6481954b308ac5e2320d2be04c93a5fd9093ade2
Biraj Pokharel <Birajpokharel11>
2022-06-10 03:45:25 +0545

    Data searialized for frntend

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .models import Product
 from  .products import products
+from .serailizers import ProductSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -16,16 +17,21 @@ def getRoutes(request):
     return Response(data)
 @api_view(['GET'])
 def getProducts(request): 
- 
-    return Response(products)
+    products = Product.objects.all() 
+    serailizer = ProductSerializer(products, many=True)
+    print('Hello',serailizer.data)
+    return Response(serailizer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i 
-    return Response(product)
+    print('Hello IK', pk)
+    product = Product.objects.get(_id=pk)
+    serailizer = ProductSerializer(product, many=False)
+
+    # for i in products:
+    #     if i['_id'] == pk:
+    #         product = i 
+    return Response(serailizer.data)
 
 
 
